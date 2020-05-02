b3fff5e4e094a144a66bef69efd8172ccda2baaa
TonyTsai <tony@unifiedpractice.com>
2020-05-02 16:27:52 +0800

    add mutiple days in same & different month

--- TDD_Dojo/BudgetService.Test/BudgetServiceTests.cs
+++ TDD_Dojo/BudgetService.Test/BudgetServiceTests.cs
@@ -1,32 +1,29 @@
 using BudgetService;
 using Moq;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace BudgetService.Test
 {
     public class BudgetServiceTests
     {
-        private readonly MockRepository mockRepository;
+        private MockRepository _mockRepository;
+        private Mock<IBudgetRepo> _mockBudgetRepo;
 
         public BudgetServiceTests()
         {
-            this.mockRepository = new MockRepository(MockBehavior.Strict);
-
-        }
-
-        private BudgetService CreateService()
-        {
-            var mockBudgetRepo = new Mock<IBudgetRepo>();
-            return new BudgetService(mockBudgetRepo.Object);
+            
         }
 
         [Fact]
         public void StartTimeOverThanEndTime_ReturnZero()
         {
             // Arrange
+            _mockRepository = new MockRepository(MockBehavior.Strict);
+            _mockBudgetRepo=_mockRepository.Create<IBudgetRepo>();
 
-            var service = this.CreateService();
+            var service = new BudgetService(_mockBudgetRepo.Object);
             DateTime start = new DateTime(2020, 05, 02);
             DateTime endTime = new DateTime(2020, 03, 15);
 
@@ -37,7 +34,84 @@ namespace BudgetService.Test
 
             // Assert
             Assert.Equal(0, result);
-            this.mockRepository.VerifyAll();
+            _mockRepository.VerifyAll();
         }
+
+        [Fact]
+        public void SameDay()
+        {
+            // Arrange
+            _mockRepository = new MockRepository(MockBehavior.Strict);
+            _mockBudgetRepo = _mockRepository.Create<IBudgetRepo>();
+            _mockBudgetRepo.Setup(x => x.GetAll()).Returns(new List<Budget>
+            {
+                new Budget(){YearMonth = "202004", Amount = 30}
+            });
+
+            var service = new BudgetService(_mockBudgetRepo.Object);
+            DateTime start = new DateTime(2020, 04, 01);
+            DateTime endTime = new DateTime(2020, 04, 01);
+
+            // Act
+            var result = service.Query(
+                start,
+                endTime);
+
+            // Assert
+            Assert.Equal(1, result);
+            _mockRepository.VerifyAll();
+        }
+        
+        [Fact]
+        public void SameMonthMultipleDay()
+        {
+            // Arrange
+            _mockRepository = new MockRepository(MockBehavior.Strict);
+            _mockBudgetRepo = _mockRepository.Create<IBudgetRepo>();
+            _mockBudgetRepo.Setup(x => x.GetAll()).Returns(new List<Budget>
+            {
+                new Budget(){YearMonth = "202004", Amount = 30}
+            });
+
+            var service = new BudgetService(_mockBudgetRepo.Object);
+            DateTime start = new DateTime(2020, 04, 01);
+            DateTime endTime = new DateTime(2020, 04, 03);
+
+            // Act
+            var result = service.Query(
+                start,
+                endTime);
+
+            // Assert
+            Assert.Equal(3, result);
+            _mockRepository.VerifyAll();
+        }
+
+        [Fact]
+        public void DifferentMonthMultipleDay()
+        {
+            // Arrange
+            _mockRepository = new MockRepository(MockBehavior.Strict);
+            _mockBudgetRepo = _mockRepository.Create<IBudgetRepo>();
+            _mockBudgetRepo.Setup(x => x.GetAll()).Returns(new List<Budget>
+            {
+                new Budget(){YearMonth = "202004", Amount = 30},
+                new Budget(){YearMonth = "202005", Amount = 310}
+            });
+
+            var service = new BudgetService(_mockBudgetRepo.Object);
+            DateTime start = new DateTime(2020, 04, 15);
+            DateTime endTime = new DateTime(2020, 05, 25);
+
+            // Act
+            var result = service.Query(
+                start,
+                endTime);
+
+            // Assert
+            Assert.Equal(266, result);
+            _mockRepository.VerifyAll();
+        }
+
     }
 }
--- TDD_Dojo/BudgetService/BudgetService.cs
+++ TDD_Dojo/BudgetService/BudgetService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace BudgetService
 {
@@ -17,11 +18,27 @@ namespace BudgetService
         {
             if (startTime > endTime) return 0;
 
-            // 1. getData
-            // 2. 攤比例
+            var data = _budgetRepo.GetAll().ToList();
+            var days = (endTime.AddDays(1) - startTime).Days;
 
+            double sum = 0;
+            for (var date = startTime.Date; date <= endTime.Date; date = date.AddDays(1))
+            {
+                sum += GetBudget(date, data);
+            }
 
-            return 0;
+            return (decimal)sum;
+        }
+
+        private double GetBudget(DateTime startTime, List<Budget> budgets)
+        {
+            var start = startTime.ToString("yyyyMM");
+            var budget = budgets.FirstOrDefault(b => b.YearMonth == start);
+            if (budget == null) return 0;
+
+            var days = DateTime.DaysInMonth(startTime.Year, startTime.Month);
+
+            return (double)budget.Amount / days;
         }
     }
 
