3012b27ab451a4d8b37fa4d7785d9f5ec63e3d0d
TonyTsai <tony@unifiedpractice.com>
2020-05-02 15:36:01 +0800

    add Moq framework & test file

--- TDD_Dojo/BudgetService.Test/BudgetServiceTests.cs
+++ TDD_Dojo/BudgetService.Test/BudgetServiceTests.cs
@@ -1,3 +1,5 @@
+using BudgetService;
+using Moq;
 using System;
 using Xunit;
 
@@ -5,10 +7,34 @@ namespace BudgetService.Test
 {
     public class BudgetServiceTests
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly MockRepository mockRepository;
 
+        public BudgetServiceTests()
+        {
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+        }
+
+        private BudgetService CreateService()
+        {
+            return new BudgetService();
+        }
+
+        [Fact]
+        public void Query_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var service = this.CreateService();
+            DateTime start = default(global::System.DateTime);
+            DateTime endTime = default(global::System.DateTime);
+
+            // Act
+            var result = service.Query(
+                start,
+                endTime);
+
+            // Assert
+            Assert.True(false);
+            this.mockRepository.VerifyAll();
         }
     }
 }
